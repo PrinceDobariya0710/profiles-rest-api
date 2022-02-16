5710254ed3ad2372ac46be80e667b71e3d400461
Prince <princekumar@bbd.co.za>
2022-02-16 11:42:00 +0530

    Added searching to user profile API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,10 +3,17 @@ from email import message
 from unicodedata import name
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 """ POST PUT and PATCH request"""
 class HelloApiView(APIView):
     """Test API View"""
@@ -105,6 +112,36 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self,request,pk=None):
         """Handle updating an object"""
         return Response({'http_method':'DELETE'})
+
+
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
